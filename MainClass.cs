e664ea1e94dd4f07de4771ed0b68f334e578deb9
KOLAGOTLA <s.kolagotla@dxc.com>
2020-12-04 19:17:11 +0530

    first commit

---
+++ MainClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MathemeticsTable
+{
+    class MainClass
+    {
+        
+        static void Main()
+        {
+            Console.Write("Enter Starting Number: ");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Last Number: ");
+            int last = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= last; i++)
+            {
+                for(int j=1;j<=5;j++)
+                {
+                    Console.WriteLine("{0}*{1}={2}",i,j,i*j);
+                }
+                Console.WriteLine("-----------------------");
+            }
+           
+        }
+    }
+}
